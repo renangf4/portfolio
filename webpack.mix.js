0e3a0715d64eeedfc90f1e2baa8adc5e4321d810
Renan Morais <renan.morais@multicredito.net.br>
2021-02-22 15:20:21 -0300

    fonts alterado para preload, inserido secao galeria e criado pagina galeria com iframe da galeria

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/pages/index.js', 'public/js')
    .sass('resources/sass/pages/index.scss', 'public/css')
+   .sass('resources/sass/includes/fonts.scss', 'public/css')
    .options({
    		processCssUrls: false
    });
